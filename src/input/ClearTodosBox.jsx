fcbef657e625b90ea895befc8267beefec8de94c
Jordan Gorrell <jgorrell1@yahoo.com>
2023-07-01 13:27:07 +0200

    Add clear confirmation as a modal

--- src/input/ClearTodosBox.jsx
+++ src/input/ClearTodosBox.jsx
@@ -1,23 +1,31 @@
 import React from 'react'
 import styles from './ClearTodosBox.module.css'
+import ReactModal from 'react-modal';
 import { useState } from 'react';
 
+ReactModal.setAppElement('#root')
+
 const ClearTodosBox = ({ clearTodos }) => {
-    const [clearTodosToggled, setClearTodosToggled] = useState(false);
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+
+    const openModal = () => setModalIsOpen(true);
+    const closeModal = () => setModalIsOpen(false);
+
+    const onConfirmClear = () => {
+        clearTodos();
+        closeModal();
+    }
 
     return (
         <div className={styles.clearTodosSection}>
-            {clearTodosToggled ? 
-            <div className={styles.confirmClearSection}>
+            <button className={styles.clearButton} onClick={openModal}>Clear Todos</button> 
+            <ReactModal isOpen={modalIsOpen} onRequestClose={closeModal} className={styles.confirmClearSection} overlayClassName={styles.modalOverlay}>
                 <span className={styles.confirmClearText}>Are you sure?</span>
                 <div>
-                    <button className={styles.confirmClearButton} onClick={clearTodos}>yes, clear my todos</button>
-                    <button className={styles.abortClearButton} onClick={() => setClearTodosToggled(false)}>no, keep my todos</button>
+                    <button className={styles.confirmClearButton} onClick={onConfirmClear}>yes, clear my todos</button>
+                    <button className={styles.abortClearButton} onClick={closeModal}>no, keep my todos</button>
                 </div>
-            </div>  
-            :
-            <button className={styles.clearButton} onClick={() => setClearTodosToggled(true)}>Clear Todos</button> 
-            }
+            </ReactModal>  
         </div>
     )
     
